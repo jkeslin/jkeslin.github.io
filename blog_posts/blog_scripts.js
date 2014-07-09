989f32e846259b11965d5647dd644a02f0a229ac
Jeff Keslin <jkeslin@gmail.com>
2014-07-08 23:09:46 -0500

    add jquery functions

---
+++ blog_posts/blog_scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+//animate name "jeffkeslin" when page opens (appear slowly)
+
+$('p').css({'color': 'blue'});
+
+$('.firstlast').on('mouseover', function(){
+	$( this ).animate({'opacity': '0')}
+});
+
+$("h1").on("mouseover", function(){
+ 	$( this ).css( "color", "orange");
+
+
+
+
+
+
+
+
+
+})
