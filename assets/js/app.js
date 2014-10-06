ef18eb13607135b0efa92dde87447d5832999d9e
Jeff Keslin <jkeslin@gmail.com>
2014-10-05 21:08:30 -0500

    basic blog links and div animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
 
+  $('#blogpostswrap').hide();
+
+  $('.blog-link').on('click', function(event){
+    event.preventDefault();
+    $('#blogpostswrap').animate({
+      height: [ "toggle", "swing" ],
+      opacity: "toggle"
+      }, 500, "linear");
+  });
 
 });
